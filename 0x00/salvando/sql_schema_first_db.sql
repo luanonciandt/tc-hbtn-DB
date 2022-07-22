6e2c1f9e450dcee610caf87c15df61bf1ba9bc64
luanonciandt <luansena@ciandt.com>
2022-07-22 12:24:39 -0300

    databases task 4

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE CLIENTE(
+  id Integer PRIMARY KEY AUTOINCREMENT,
+  nome VARCHAR(80) NOT NULL,
+  email VARCHAR(40) NOT NULL,
+  telefone INTEGER
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
