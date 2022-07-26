1b68482acb7b76504cd507de9947807d3633e386
luanonciandt <luansena@ciandt.com>
2022-07-26 13:24:16 -0300

    java jdbc part 2 task 2

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE clientes(
+  id Integer PRIMARY KEY AUTOINCREMENT,
+  nome varchar(255) not NULL,
+  idade Integer not NULL,
+  cpf varchar(10) not NULL,
+  rg varchar(7) not NULL
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
